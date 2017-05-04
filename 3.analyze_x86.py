837b66e65291dc9ee2ba1775455f07411d0b2582
lfd <617390727@qq.com>
2017-05-04 18:15:22 +0800

    update

--- 3.analyze_x86.py
+++ 3.analyze_x86.py
@@ -3,12 +3,12 @@ from gem5_utils import parse_result, to_csv, generate_plot
 
 # Define benchmark names.
 benchmarks = [
-      #'400.perlbench',
+      '400.perlbench',
       '401.bzip2',
-      #'403.gcc',
-      #'410.bwaves',
-      #'416.gamess',
-      #'429.mcf',
+      '403.gcc',
+      '410.bwaves',
+      '416.gamess',
+      '429.mcf',
       #'433.milc',
       #'434.zeusmp',
       #'435.gromacs',
@@ -32,44 +32,52 @@ benchmarks = [
 
 
 # Create CSVs and Figures illustrating the relationship between the L2 Cache Size and the L2 Miss Rate.
-def parse_results_l2_sizes():
+def parse_results_l2_prefetchers():
     results = []
 
     for benchmark in benchmarks:
-        # for l2_size in ['256kB', '512kB', '1MB', '2MB', '4MB', '8MB']:
-        # for l2_size in ['256kB', '512kB']:
-        for l2_size in ['256kB']:
-            results.append(
-                parse_result('results/' +
-                             benchmark + '/' + l2_size + '/8way/4c/',
-                             benchmark=benchmark,
-                             l2_size=l2_size)
-            )
+        for l2_size in ['1MB']:
+            for l2_prefetcher in ['stride', 'tagged']:
+                results.append(
+                    parse_result('results/' +
+                                benchmark + '/' + l2_size + '/8way/l2_' + l2_prefetcher + '/1c/',
+                                benchmark=benchmark,
+                                l2_size=l2_size,
+                                l2_prefetcher=l2_prefetcher)
+                )
 
-    to_csv('results/l2_sizes.csv', results, [
+    to_csv('results/l2_prefetchers.csv', results, [
         ('Benchmark', lambda r: r.props['benchmark']),
-        ('L2 Size', lambda r: r.props['l2_size']),
+        # ('L2 Size', lambda r: r.props['l2_size']),
+        ('L2 Prefetcher', lambda r: r.props['l2_prefetcher']),
+        # ('L1D Prefetcher', lambda r: r.props['l1d_prefetcher']),
+        ('L2 Prefetches', lambda r: r.stats[0]['system.l2.prefetcher.num_hwpf_issued']),
+        # ('L1D Prefetches', lambda r: r.stats[0]['system.cpu.dcache.prefetcher.num_hwpf_issued']),
         ('L2 Miss Rate', lambda r: r.stats[0]['system.l2.overall_miss_rate::total']),
-        ('# Cycles', lambda r: r.stats[0]['system.switch_cpus0.numCycles'])
+        # ('L1D Miss Rate', lambda r: r.stats[0]['system.cpu.dcache.overall_miss_rate::total']),
+
+        ('# Cycles', lambda r: r.stats[0]['system.switch_cpus.numCycles'])
     ])
 
-    generate_plot('results/l2_sizes.csv',
-                  'results/l2_sizes_vs_l2_miss_rate.pdf', 'Benchmark', 'L2 Miss Rate',
-                  'L2 Size', 'L2 Miss Rate')
-    generate_plot('results/l2_sizes.csv',
-                  'results/l2_sizes_vs_num_cycles.pdf', 'Benchmark', '# Cycles',
-                  'L2 Size', '# Cycles')
+    # generate_plot('results/l2_prefetchers.csv',
+    #               'results/l2_sizes_vs_l2_miss_rate.pdf', 'Benchmark', 'L2 Miss Rate',
+    #               'L2 Size', 'L2 Miss Rate')
+    # generate_plot('results/l2_prefetchers.csv',
+    #               'results/l2_sizes_vs_num_cycles.pdf', 'Benchmark', '# Cycles',
+    #               'L2 Size', '# Cycles')
+
+    generate_plot('results/l2_prefetchers.csv',
+                  'results/l2_prefetchers_vs_l2_prefetches.pdf', 'Benchmark', 'L2 Prefetches',
+                   'L2 Prefetcher', 'L2 Prefetches')
+    generate_plot('results/l2_prefetchers.csv',
+                  'results/l2_prefetchers_vs_l2_miss_rate.pdf', 'Benchmark', 'L2 Miss Rate',
+                   'L2 Prefetcher', 'L2 Miss Rate')
+    generate_plot('results/l2_prefetchers.csv',
+                  'results/l2_prefetchers_vs_num_cycles.pdf', 'Benchmark', '# Cycles',
+                   'L2 Prefetcher', '# Cycles')
 
     return results
 
 
-# Create CSVs and Figures illustrating the relationship between the L2 Cache Size and the L2 Miss Rate.
-results_l2_sizes = parse_results_l2_sizes()
-
-# Sample Usage: access configs from parsed results
-print('system.l2.size: ' + str(results_l2_sizes[0].configs['system.l2.size']))
-
-# Sample Usage: access stats from parsed results.
-# Note: Here, the stats are an array of dictionaries, which corresponds to different program phases.
-print('[phase ' + str(0) + '] system.switch_cpus0.numCycles: ' + results_l2_sizes[0].stats[i][
-    'system.switch_cpus0.numCycles'])
+# Create CSVs and Figures illustrating the relationship between the L2 Prefetchers and the L2 Miss Rate.
+parse_results_l2_prefetchers()
